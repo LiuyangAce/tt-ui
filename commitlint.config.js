8c7eef7d271cb45d6d6d2037525b88c3a1f42b9a
liuyang <yliu@in-road.com>
2023-02-24 15:18:34 +0800

    chore: 添加commitlint、husky来强制检查提交信息是否符合规范

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+const types = [
+  'feat', // 新功能 feature
+  'fix', // 修复 bug
+  'docs', // 文档注释
+  'style', // 代码格式(不影响代码运行的变动)
+  'refactor', // 重构(既不增加新功能，也不是修复bug)
+  'perf', // 性能优化
+  'test', // 增加测试
+  'chore', // 构建过程或辅助工具的变动
+  'revert', // 回退
+  'build' // 打包
+]
+
+const commitTypeRules = [
+  2, 'always', types
+]
+
+module.exports = { 
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    "type-enum": commitTypeRules
+  }
+};
