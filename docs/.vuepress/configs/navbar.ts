891a9183f43e563b836cd4a352770b277bce43f2
liuyang <1773587692@qq.com>
2023-03-13 17:46:06 +0800

    docs: 修改navbar

--- docs/.vuepress/configs/navbar.ts
+++ docs/.vuepress/configs/navbar.ts
@@ -5,6 +5,15 @@ export default [
   { text: 'API 参考', link: '/api/' },
   {
     text: '更新日志',
-    link: 'https://gitee.com/NewCoderMCL/vuepress-ui',
+    children: [
+      {
+        text: 'gitee',
+        link: 'https://gitee.com/LiuyangAce/tt-ui'
+      },
+      {
+        text: 'github',
+        link: 'https://github.com/LiuyangAce/tt-ui'
+      }
+    ]
   },
 ]
